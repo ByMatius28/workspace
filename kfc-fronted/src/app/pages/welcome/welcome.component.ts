5d13bcb649f2290ac7bad6c5672c184c33c938c6
David Correa <dacorrea@sudamericano.edu.ec>
2024-06-12 18:19:39 -0500

    Update

--- kfc-fronted/src/app/pages/welcome/welcome.component.ts
+++ kfc-fronted/src/app/pages/welcome/welcome.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuFormComponent } from '../../components/menu-form/menu-form.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
-
+  imports: [MenuFormComponent],
 })
 export class WelcomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  ngOnInit() {}
 }
